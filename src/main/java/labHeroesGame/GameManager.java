91a22f9f629bee11ef548858ded76d8b7ac094e1
philipshurpik <philip.shurpik@gmail.com>
2017-10-21 22:54:38 +0300

    add friendship :)

--- src/main/java/labHeroesGame/GameManager.java
+++ src/main/java/labHeroesGame/GameManager.java
@@ -20,15 +20,19 @@ class GameManager {
     }
 
     private void fight(Character c1, Character c2) throws InterruptedException {
-        int a = 0;
         String c1Name = c1.getClass().getSimpleName();
         String c2Name = c2.getClass().getSimpleName();
         c2Name = c1Name.equals(c2Name) ? c2Name + " Junior" : c2Name;
         System.out.printf("Welcome on the Ring!! %s vs %s\n", c1Name, c2Name);
         System.out.printf("%s has %d power and %d hp\n", c1Name, c1.getPower(), c1.getHp());
         System.out.printf("%s has %d power and %d hp\n", c2Name, c2.getPower(), c2.getHp());
+
+        if (c1.getPower() == c2.getPower() && c1.getPower() == 0) {
+            System.out.println("Friendship wins!");
+            return;
+        }
+
         do {
-            a++;
             int index = random.nextInt(2);
             Character first = index == 1 ? c1 : c2;
             String firstName = index == 1 ? c1Name : c2Name;
@@ -38,11 +42,11 @@ class GameManager {
             first.kick(second);
             if (second.isAlive()) {
                 Thread.sleep(500);
-                System.out.printf("%s kicks %s, but %s is still alive and has %d hp\n", firstName, secondName, secondName, second.getHp());
+                System.out.printf("%s kicks %s, but %s is still alive and has %d hp and %d power\n", firstName, secondName, secondName, second.getHp(), second.getPower());
                 second.kick(first);
 
                 if (first.isAlive()) {
-                    System.out.printf("%s hits %s... %s survives and has %d hp\n", secondName, firstName, firstName, first.getHp());
+                    System.out.printf("%s hits %s... %s survives and has %d hp and %d power\n", secondName, firstName, firstName, first.getHp(), first.getPower());
                 } else {
                     System.out.printf("%s hits back and WIN!!!\n", secondName);
                 }
@@ -52,6 +56,6 @@ class GameManager {
             }
             Thread.sleep(1000);
         }
-        while (c1.isAlive() && c2.isAlive() && a < 50);
+        while (c1.isAlive() && c2.isAlive());
     }
 }
