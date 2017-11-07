ac71f29a1afefd1a8cadc70a02118b042e31fb11
philipshurpik <philip.shurpik@gmail.com>
2017-11-07 21:56:45 +0200

    hw2 - objectfactory moved to method

--- src/main/java/hw2/objectFactory/ObjectFactory.java
+++ src/main/java/hw2/objectFactory/ObjectFactory.java
@@ -4,6 +4,7 @@ import hw2.objectFactory.config.Config;
 import hw2.objectFactory.config.JavaConfig;
 import lombok.SneakyThrows;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Random;
 
@@ -16,30 +17,36 @@ public class ObjectFactory {
         return ourInstance;
     }
 
-    private ObjectFactory() {
-    }
+    private ObjectFactory() { }
 
     @SneakyThrows
     public <T> T createObject(Class<T> type) {
         if (type.isInterface()) {
            type =  config.getImpl(type);
         }
-        T o = type.newInstance();
+        T instance = type.newInstance();
 
         Field[] fields = type.getDeclaredFields();
         for (Field field : fields) {
+            Annotation[] annotations = field.getAnnotations();
+            for (Annotation annotation : annotations) {
+                System.out.println(annotation.toString());
 
-            InjectRandomInt annotation = field.getAnnotation(InjectRandomInt.class);
-            if (annotation != null) {
-                int min = annotation.min();
-                int max = annotation.max();
-                int randomIntValue = random.nextInt(max - min) + min;
-                field.setAccessible(true);
-                field.set(o,randomIntValue);
-
+                Annotation fieldAnnotation = field.getAnnotation(annotation.annotationType());
+                if (fieldAnnotation != null) {
+                    configureAnnotation(instance, field, (InjectRandomInt) fieldAnnotation);
+                }
             }
         }
+        return instance;
+    }
 
-        return o;
+    @SneakyThrows
+    private void configureAnnotation(Object instance, Field field, InjectRandomInt annotation) {
+        int min = annotation.min();
+        int max = annotation.max();
+        int randomIntValue = random.nextInt(max - min) + min;
+        field.setAccessible(true);
+        field.set(instance, randomIntValue);
     }
 }
